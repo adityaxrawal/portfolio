1209d60da8c0475b4e797a403c35b5893df68ea3
Aditya Rawal <ar.adityarawal@gmail.com>
2024-10-25 10:54:45 +0530

    Working on the structure of the project section

--- src/component/Content/Project/projects.component.js
+++ src/component/Content/Project/projects.component.js
@@ -4,33 +4,18 @@ import './projects.component.css'
 const Projects = () => {
     const sectionRef = useRef(null);
     const [isPinned, setIsPinned] = useState(false);
-    const [scrollIndex, setScrollIndex] = useState(0);
-    const MAX_TEXTS_INSIDE_SCROLL = 2
 
-    // Handle scroll event and calculate the index of the current text
     const handleScroll = () => {
         if (!sectionRef.current) return;
 
         const sectionTop = sectionRef.current.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        if (sectionTop <= 0 && Math.abs(sectionTop) < windowHeight * (MAX_TEXTS_INSIDE_SCROLL + 1)) {
-            setIsPinned(true);
-            console.log('--------------start--------------')
-            console.log('section top', sectionTop)
-            console.log('window height', windowHeight)
-            const scrollPosition = Math.min(
-                Math.floor((Math.abs(sectionTop) / windowHeight) * (MAX_TEXTS_INSIDE_SCROLL + 1)),
-                MAX_TEXTS_INSIDE_SCROLL
-            ); // Max scroll index is 2 in this example
-            console.log('math.abs', (Math.abs(sectionTop) / windowHeight))
-            console.log('scroll position', scrollPosition)
-            console.log('----------------end--------------')
-            setScrollIndex(scrollPosition);
-        } else {
-            setIsPinned(false);
+        const sectionOffSetHeight = sectionRef.current.offsetHeight;
+        if (sectionTop < 0) {
+            setIsPinned(true)
+        } else if (sectionTop === -sectionOffSetHeight) {
+            setIsPinned(false)
         }
-    };
+    }
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
@@ -42,15 +27,20 @@ const Projects = () => {
 
     return (
         <React.Fragment>
-            <section ref={sectionRef} className='project-section'>
-                <div className='project-header' style={{ position: isPinned ? "fixed" : "relative", maxWidth: isPinned ? '80%' : '100%', padding: isPinned ? '0% 10%' : '0%' }}>
-                    <div className='project-container-header'>
-                        <span className='project-header-text'>Projects</span>
+            <section ref={sectionRef} className='project-section' style={{ position: isPinned ? 'sticky' : 'intial', top: 0 }}>
+                <div className='project-container'>
+                    <div className='project-heading'>
+                        <span className='project-heading-text'>Projects</span>
                     </div>
-                    <div className='project-container-text'>
-                        {scrollIndex === 0 && <span className='project-text'>Text 1</span>}
-                        {scrollIndex === 1 && <span className='project-text'>Text 2</span>}
-                        {scrollIndex === 2 && <span className='project-text'>Text 3</span>}
+                    <div className='project-container-text-images'>
+                        <div className='project-container-text'>
+                            <span className='project-text'>Text 1</span>
+                            <span className='project-text'>Text 2</span>
+                            <span className='project-text'>Text 3</span>
+                        </div>
+                        <div className='project-container-images' style={{ position: isPinned ? 'sticky' : 'initial', top: 0 }}>
+                            <span className='project-image'>image1</span>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -59,3 +49,30 @@ const Projects = () => {
 }
 
 export default Projects
+
+// style={{ position: isPinned ? "fixed" : "relative", maxWidth: isPinned ? '80%' : '100%', padding: isPinned ? '0% 10%' : '0%' }}
+
+// Handle scroll event and calculate the index of the current text
+// const handleScroll = () => {
+//     if (!sectionRef.current) return;
+
+//     const sectionTop = sectionRef.current.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
+
+//     if (sectionTop <= 0 && Math.abs(sectionTop) < windowHeight * (MAX_TEXTS_INSIDE_SCROLL + 1)) {
+//         setIsPinned(true);
+//         console.log('--------------start--------------')
+//         console.log('section top', sectionTop)
+//         console.log('window height', windowHeight)
+//         const scrollPosition = Math.min(
+//             Math.floor((Math.abs(sectionTop) / windowHeight) * (MAX_TEXTS_INSIDE_SCROLL + 1)),
+//             MAX_TEXTS_INSIDE_SCROLL
+//         ); // Max scroll index is 2 in this example
+//         console.log('math.abs', (Math.abs(sectionTop) / windowHeight))
+//         console.log('scroll position', scrollPosition)
+//         console.log('----------------end--------------')
+//         setScrollIndex(scrollPosition);
+//     } else {
+//         setIsPinned(false);
+//     }
+// };
