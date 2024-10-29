af7cfe2fe27e19ce13592239dcad15047425b566
Aditya Rawal <ar.adityarawal@gmail.com>
2024-10-29 23:38:04 +0530

    Made the working structure of the project scroll behaviour

--- src/component/Content/Project/projects.component.js
+++ src/component/Content/Project/projects.component.js
@@ -3,17 +3,24 @@ import './projects.component.css'
 
 const Projects = () => {
     const sectionRef = useRef(null);
+    const imagesRef = useRef(null);
+    const textRef = useRef(null);
     const [isPinned, setIsPinned] = useState(false);
 
     const handleScroll = () => {
-        if (!sectionRef.current) return;
+        if (!sectionRef.current || !imagesRef.current) return;
 
         const sectionTop = sectionRef.current.getBoundingClientRect().top;
         const sectionOffSetHeight = sectionRef.current.offsetHeight;
+        const textTop = textRef.current.getBoundingClientRect().top;
+
         if (sectionTop < 0) {
             setIsPinned(true)
+            imagesRef.current.scrollTop = parseInt(Math.abs(textTop) / 1.75);
         } else if (sectionTop === -sectionOffSetHeight) {
             setIsPinned(false)
+        } else {
+            imagesRef.current.scrollTop = 0;
         }
     }
 
@@ -27,19 +34,21 @@ const Projects = () => {
 
     return (
         <React.Fragment>
-            <section ref={sectionRef} className='project-section' style={{ position: isPinned ? 'sticky' : 'intial', top: 0 }}>
+            <section ref={sectionRef} className='project-section'>
+                <div className='project-heading' style={{ position: isPinned ? 'sticky' : 'relative', top: 0 }}>
+                    <span className='project-heading-text'>Projects</span>
+                </div>
                 <div className='project-container'>
-                    <div className='project-heading'>
-                        <span className='project-heading-text'>Projects</span>
+                    <div className='project-container-text' ref={textRef}>
+                        <span className='project-text'>Text 1</span>
+                        <span className='project-text'>Text 2</span>
+                        <span className='project-text'>Text 3</span>
                     </div>
-                    <div className='project-container-text-images'>
-                        <div className='project-container-text'>
-                            <span className='project-text'>Text 1</span>
-                            <span className='project-text'>Text 2</span>
-                            <span className='project-text'>Text 3</span>
-                        </div>
-                        <div className='project-container-images' style={{ position: isPinned ? 'sticky' : 'initial', top: 0 }}>
-                            <span className='project-image'>image1</span>
+                    <div className='project-container-images' style={{ position: isPinned ? 'sticky' : 'relative', top: 0 }}>
+                        <div className='project-images' ref={imagesRef} style={{ overflow: isPinned ? 'scroll' : 'hidden' }}>
+                            <span className='project-images-items' style={{ backgroundColor: "darkred" }}>image 1</span>
+                            <span className='project-images-items' style={{ backgroundColor: "darkblue" }}>image 2</span>
+                            <span className='project-images-items' style={{ backgroundColor: "darkgreen" }}>image 3</span>
                         </div>
                     </div>
                 </div>
