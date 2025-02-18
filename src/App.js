ca3310a445a886e7d7b20dbc9c3001d6e6c17a8f
Aditya Rawal <aditya.rawal@lsq.net>
2025-02-18 14:14:19 +0530

    added the smooth scroll but the work component scroll is affect and not fixed yet.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import './App.css';
 import Page from './component/page.component';
 import { SharedStateContext, SharedStateProvider } from './component/shared-state-context-api';
+import { useSmoothScroll } from './share/hooks/smoothScroll.customHook';
 
 
 
@@ -15,19 +16,20 @@ function App() {
 
 function ThemedApp() {
   const { isDarkTheme, backgroundColor } = useContext(SharedStateContext);
-  
+  const scrollRef = useSmoothScroll();
+
   const darkTheme = {
     backgroundColor: backgroundColor,
     color: 'white'
   }
-  
+
   const lightTheme = {
     backgroundColor: backgroundColor,
     color: 'black'
   }
 
   return (
-    <div className="App" style={isDarkTheme ? darkTheme : lightTheme}>
+    <div ref={scrollRef} className="App" style={isDarkTheme ? darkTheme : lightTheme}>
       {/* <span style={{position: 'sticky', top: 0, backgroundColor: 'white', color: 'black', display: 'flex', justifyContent: 'center', alignContent: 'center'}}>current BCG: {backgroundColor}</span> */}
       <Page />
     </div>
