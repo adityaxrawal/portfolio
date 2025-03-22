4c01f31678ea5e9f8bec2afeadd02ecaffa05d79
Aditya Rawal <ar.adityarawal@gmail.com>
2025-03-23 00:43:33 +0530

    completed the responsiveness of header, herosection, workexperience for all screen sizes

--- src/share/utils/constant.js
+++ src/share/utils/constant.js
@@ -4,12 +4,12 @@ export const links = {
 }
 
 export const darkModeColorList = {
-    0: '#1B1F23',  // Darkest shade (near-black with a hint of blue)
-    1: '#2E3A47',  // Dark blue-gray, similar depth to light mode's shade 5
-    2: '#445363',  // Muted mid-tone blue-gray
-    3: '#62798D',  // Softer blue-gray, matches light mode’s mid-tones
-    4: '#829AB1',  // Lighter muted blue, for highlights
-    5: '#DDE3EC'   // Lightest shade, off-white with a hint of blue
+    0: '#222831',  // Darkest shade (near-black with a hint of blue)
+    1: '#31363F',  // Dark blue-gray, similar depth to light mode's shade 5
+    2: '#76ABAE',  // Muted mid-tone blue-gray
+    3: '#EEEEEE',  // Softer blue-gray, matches light mode’s mid-tones
+    4: '#B8E0E3',  // Lighter muted blue, for highlights
+    5: '#F0F5F7'   // Lightest shade, off-white with a hint of blue
 }
 
 
@@ -21,3 +21,42 @@ export const lightModeColorList = {
     4: '#3F72AF', // Medium-dark blue
     5: '#112D4E'  // Darkest blue
 }
+
+
+export const WorkExperience = [
+    {
+        companyName: 'Leadsquared',
+        companyLink: 'https://www.leadsquared.com/',
+        title: 'Software Engineer',
+        years: 'Dec 2023 - Present',
+        description: `
+            Developed a serverless web app using AWS Lambda, cutting costs and boosting speed. 
+            Built RESTful APIs in Node.js and Express.js, handling 500K requests monthly. 
+            Integrated third-party APIs for scalability. 
+            Led code reviews for security and maintainability. 
+            Worked with teams to enhance features and client satisfaction.`
+    },
+    {
+        companyName: 'Uniq Global Labs',
+        companyLink: 'https://www.develup.com/',
+        title: 'Software Engineer',
+        years: 'Jan 2023 - Dec 2023',
+        description: `
+            Built and maintained DevelUp’s site using Next.js, React.js, and Express.js, improving page load by 45%. 
+            Created web pages boosting engagement by 30%. 
+            Designed a job search board, enhancing accuracy by 40%. 
+            Built a Resume Builder used by 5,000+ users. 
+            Developed a dashboard integrating job listings, enrollments, and profiles for retention.`
+    },
+    {
+        companyName: 'Wipro',
+        companyLink: 'https://www.wipro.com/',
+        title: 'Software Engineer',
+        years: 'Feb 2022 - Aug 2022',
+        description: `
+            Developed an e-commerce platform with MEAN stack, handling database and frontend. 
+            Designed a coupon microservice for sales tracking. 
+            Created REST APIs for user authentication and product management. 
+            Focused on secure and scalable architecture, ensuring seamless integration across features for enhanced user experience.`
+    }
+]
