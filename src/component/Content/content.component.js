1d6b38593a19858925d4b1da04062ad344f261e7
Aditya Rawal <ar.adityarawal@gmail.com>
2025-03-25 12:13:16 +0530

    optimised the images count and organised the folder

--- src/component/Content/content.component.js
+++ src/component/Content/content.component.js
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import HeroSection from './HeroSection/heroSection.component'
 import Work from './Work/work.component'
 import Technology from './Tech/tech.component'
 
-const Content = (props) => {
-
-  const [allImagesLoaded, setAllImagesLoaded] = useState(0)
-
-  useEffect(() => {
-    console.log('count of all images loaded', allImagesLoaded)
-    if (allImagesLoaded === 21) {
-      console.log('all images are loaded', allImagesLoaded)
-      props.handleLoading(false)
-    } else {
-      console.log('else part')
-      props.handleLoading(true)
-    }
-  }, [allImagesLoaded, props])
-
-  const handleLoadImage = () => {
-    setAllImagesLoaded(allImagesLoaded + 1)
-  };
-
-
+const Content = () => {
   return (
     <React.Fragment>
-      <HeroSection handleLoadImage={handleLoadImage} />
+      <HeroSection />
       <Work />
       <Technology />
     </React.Fragment>
