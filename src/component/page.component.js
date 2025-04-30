78b6785dc769e21c0119c7cc3d55f8de49b1af33
Aditya Rawal <ar.adityarawal@gmail.com>
2025-04-30 21:57:30 +0530

    Added initial phase of footer with a structure

--- src/component/page.component.js
+++ src/component/page.component.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import './page.component.css'
-import Header from './Header/header.component'
-import Footer from './Footer/footer.component'
-import Content from './Content/content.component'
+import React from "react";
+import "./page.component.css";
+import Header from "./Header/header.component";
+import Footer from "./Footer/footer.component";
+import Content from "./Content/content.component";
 
 const Page = () => {
-    return (
-        <React.Fragment>
-            <div className='page'>
-                <Header />
-                <Content />
-                <Footer />
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div className="page">
+        <Header />
+        <Content />
+      </div>
+      <div className="page-footer">
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default Page
+export default Page;
