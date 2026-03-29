7d73b7ecb07e16c4020d4866af55149ac0823a21
Aditya Rawal <ar.adityarawal@gmail.com>
2026-03-29 14:26:21 +0530

    refactor: migrate shared components and hooks to src/shared

--- src/component/Content/Modal/ContactForm/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as ContactForm } from "./ContactForm.component";
-export { default as FormField } from "./FormField.component";
-export { default as SubmitButton } from "./SubmitButton.component";
