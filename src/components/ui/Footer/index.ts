ed286af2bcc8f8e9874e7e16e0edeffeda1b637b
Aditya Rawal <ar.adityarawal@gmail.com>
2026-05-28 14:44:01 +0530

    feat(footer): implement Footer v3 featuring interactive D3 world map

--- src/components/ui/Footer/index.ts
+++ src/components/ui/Footer/index.ts
@@ -1,3 +1,4 @@
-export { default } from './v2/Footer';
+export { default } from './v3/Footer';
 export { default as FooterV1 } from './v1/Footer';
 export { default as FooterV2 } from './v2/Footer';
+export { default as FooterV3 } from './v3/Footer';
